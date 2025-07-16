46ded55744cd827d5983f7f5671a37ab84cb8937
Anatole CURLIER <anatole.curlier@gmail.com>
2025-07-16 16:46:15 +0200

    Remove useless hooks

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,10 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit'
-import todoReducer from './features/todos/todosSlice.ts'
 import {todosApi} from "./services/todos.ts";
 
 export const store = configureStore({
     reducer: {
-        todos: todoReducer,
         [todosApi.reducerPath]: todosApi.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todosApi.middleware),
