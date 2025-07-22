c65d829542fd9ed985f0963a2f0e0568b37e4022
Anatole CURLIER <anatole.curlier@gmail.com>
2025-07-23 00:28:54 +0200

    Add no todos found in todos list

--- client/src/features/todos/components/containers/TodosList.tsx
+++ client/src/features/todos/components/containers/TodosList.tsx
@@ -2,6 +2,7 @@ import type {FC} from "react";
 import {CircularProgress, Stack} from "@mui/material";
 import {useGetTodosQuery} from "../../services/todos.ts";
 import {TodoItem} from "../ui/TodoItem.tsx";
+import {NoTodos} from "../ui/NoTodos.tsx";
 
 export const TodosList: FC = () => {
     const {data, isLoading} = useGetTodosQuery(undefined)
@@ -14,6 +15,7 @@ export const TodosList: FC = () => {
 
     return (
         <Stack direction={'column'} spacing={2}>
+            {data?.length === 0 && (<NoTodos />)}
             {data?.map(todo => <TodoItem key={todo.id} todo={todo}/>)}
         </Stack>
     )
